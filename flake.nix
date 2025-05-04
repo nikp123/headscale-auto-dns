5788cf975e20d5ffab9186d4576a5a016bb7ec6f
nikp123 <nikp123@e.email>
2025-05-05 00:27:57 +0200

    Add nix flakes (for convenience sake)

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Automatic Headscale DNS generator based on Traefik server state";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        });
+    in rec {
+      overlay = final: prev: {
+        headscale-auto-dns = final.rustPlatform.buildRustPackage {
+          pname = "headscale-auto-dns";
+          version = "unstable";
+          description = "Automatic Headscale DNS generator based on Traefik server state";
+
+          nativeBuildInputs = with final; [ gcc pkg-config ];
+
+          buildInputs = with final; [ openssl ]; 
+
+          src = self;
+
+          cargoLock = { lockFile = self + "/Cargo.lock"; };
+
+          CARGO_FEATURE_USE_SYSTEM_LIBS = "1";
+        };
+      };
+
+      packages =
+        forAllSystems (system: { inherit (nixpkgsFor.${system}) headscale-auto-dns; });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.headscale-auto-dns);
+
+      apps = forAllSystems (system: {
+        headscale-auto-dns = {
+          type = "app";
+          program = "${self.packages.${system}.headscale-auto-dns}/bin/headscale-auto-dns";
+        };
+      });
+
+      defaultApp = forAllSystems (system: self.apps.${system}.headscale-auto-dns);
+
+      devShell = forAllSystems (system:
+        nixpkgs.legacyPackages.${system}.mkShell {
+          inputsFrom = builtins.attrValues (packages.${system});
+        });
+    };
+}
+
